1686e024c8308b652d08cf2990e5c1fb474ccb77
Robert Watts <robertwatts@Roberts-MacBook-Pro.local>
2017-01-03 19:52:57 -0500

    laid groundwork for javascript

--- scriptz.js
+++ scriptz.js
@@ -9,17 +9,21 @@ $( document ).ready(function() {
 //every API key is the same with Giphy, so I don't really see the need to use 
 
 //starting list of  pre-loaded gifs
-var starterGifs = ["Star Wars", "Nintendo", "The Office", "Simpsons"];               
-
-//hopefully, our display gif Info Function re-renders the HTML to display the appropriate content
-var searchWord;
+var gifs = ["Star Wars", "Nintendo", "The Office", "Simpsons"];               
 
 
-var queryURLBase = "http://api.giphy.com/v1/gifs/search?q=" + searchWord + "&api_key=dc6zaTOxFJmzC&limit=10";   
+
+
+  
 
 var gifCounter = 0;
 
-function runQuery(){
+function displayGIFs(){
+
+var searchQuery = $(this).attr("data-name");
+
+var queryURLBase = "http://api.giphy.com/v1/gifs/search?q=" + searchQuery + "&api_key=dc6zaTOxFJmzC&limit=10";
+
 
 $.ajax({
           url: queryURLBase,
@@ -27,28 +31,102 @@ $.ajax({
         })
 
        .done(function(response) {
-       	console.log(queryURLBase);
 
-       	console.log(response);
+        var gifDiv = $("<div class'gif'>");
+
+
+      
+
+        // Storing the rating data
+          var rating = response.data.rating;
+
+          // Creating an element to have the rating displayed
+         var pOne = $("<p>").text("Rating: " + rating);
+
+        // Displaying the rating
+       gifDiv.append(pOne);
+
+
+       //retrieving the url for the image
+       var imgUrl = response.data.image_original_url;
+
+       //creating an element to hold the image
+       var image = $("<img>").attr("src", imgURL);
+
+       //appending the image
+       gifDiv.append(image);
+
+
+        console.log(queryURLBase);
+
+        console.log(response);
+
+        console.log(response.data.rating);  
        }) 
 
 }
+//function for displaying gif data (image and ratings)
+function renderButtons() {
 
- $('#searchButton').on('click', function(event) {
+ $("#buttons-view").empty();
+//looping through the array of gifs
 
- 	
+for (var i = 0; i < gifs.length; i++) {
 
-    console.log("im looking for data rn..");
+  //now, we dynamically generate buttons for each movie in the array
+  //the code $("<button>") is all jQuery needs to create the beginning and end tag
 
-    $("#resultsArea").empty();
+var a = $("<button>");
 
-    searchWord = $('#searchTerm').val().trim();  
+// Adding a class of gif to our button
+a.addClass("gif");
+ // Adding a data-attribute
+a.attr("data-name", gifs[i]);
+
+// Providing the initial button text
+a.text(gifs[i]);
 
 
-console.log(queryURLBase); 
-   
+//adding the button to the buttons-view div
 
-    })   
+$("#buttons-view").append(a);
 
- console.log(queryURLBase); 
+//providing the initial button text
 
+a.text(gifs[i]);
+
+// Adding the button to the buttons-view div
+$("#buttons-view").append(a);
+
+
+}
+
+}  
+
+//this here function handles events where a movie button is clicked
+
+$("#add-GIF").on("click", function(event) {
+  event.preventDefault();
+
+  //this line will grab the input from the textbox
+
+  var movie = $("#GIF-input").val().trim();
+
+
+    //adding gif from the textbox to array
+
+  gifs.push(movie);
+
+  //calling renderButtons, which handles the processing of our movie array
+
+  renderButtons();
+});
+
+
+//adding a click event listener to all elements with a class of "gif"
+
+$(document).on("click", ".movie", displayGIFs);
+
+//calling the renderButtons function to display the initial buttons
+
+renderButtons();
